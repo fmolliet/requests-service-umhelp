2face7f0107a852ede1bb2dcc469a19345e5b7c1
fmolliet <fmolliet@hotmail.com>
2020-03-08 12:59:18 -0300

    Removendo arquitetura antiga

--- .old/src/models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const OrdersSchema = new mongoose.Schema({
-    id_requester: mongoose.ObjectId,
-    date: { type: Date, default: Date.now },
-    duration: Number,
-})
-
-module.exports = mongoose.model('Orders', OrdersSchema);
